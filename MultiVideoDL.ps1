7c9af40427a116e20672d01902297f7439774214
MavSterling <35612044+MavSterling@users.noreply.github.com>
2019-06-07 20:36:48 -1000

    Uploading the Youtube-DL scripts written at home
    In all of my infinite stupidity, I have written the youtube-dl execution portion at home, but forgot to create a repository of the actual web scraper. So this is the part 1 of who the fuck knows.

---
+++ MultiVideoDL.ps1
@@ -0,0 +1,8 @@
+﻿$CSV = Read-Host -Prompt 'CSV Filename:'
+$Content = Import-Csv $CSV
+ForEach ($URL in $Content) {
+    $video = $URL.URL
+    Write-Host "Downloading Video from: '$video'"
+    & youtube-dl.exe -o "%(title)s.%(ext)s" $video --no-overwrites -c
+}
+
