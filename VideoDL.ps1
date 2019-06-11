215e3fc0eae462f44ad7157315e70091db116996
MavSterling <35612044+MavSterling@users.noreply.github.com>
2019-06-10 17:15:25 -1000

    Deleting unused testing script

--- VideoDL.ps1
+++
@@ -1,5 +0,0 @@
-﻿$URL = Read-Host -Prompt 'Video URL'
-Write-Host "Downloading Video from: '$URL'"
-& youtube-dl.exe -o "%(series)s/%(title)s.%(ext)s" --no-overwrites -c --batch-file 
-
-youtube-dl.exe -o "%(series)s/%(title)s.%(ext)s" --no-overwrites -c --batch-file ".\videos.csv" --max-sleep-interval 30 --min-sleep-interval 5
